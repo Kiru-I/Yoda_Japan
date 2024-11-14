50f49064b097b7529175ac51a5974806d6052cda
Kiru-I <julianbenhart@gmail.com>
2024-11-14 20:41:10 +0700

    animation index.php

--- index.php
+++ index.php
@@ -24,33 +24,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Log In Cafekita</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 <body>
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h1 class="card-title test-center mb-4">LOGIN</h1>
-                        <form action="" method="post">
-                            <div class="mb-3">
-                                <label for="username" class="form-label">Username</label>
-                                <input type="text" name="username" id="username" class="form-control" required>
-                            </div>
-                            <div class="mb-3">
-                                <label for="password" class="form-label">Password</label>
-                                <input type="text" name="password" id="password" class="form-control" required>
-                            </div>
-                            <div class="d-grid">
-                                <button type="submit" class="btn btn-primary mb-2">Log In</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+    <div class="box">
+        <span class="borderLine"></span>
+        <form action="" method="POST">
+            <h2>Sign In</h2>
+            <div class="inputBox">
+                <input type="text"  name="username" id="username" required>
+                <span>Username</span>
+                <i></i>
             </div>
-        </div>
+            <div class="inputBox">
+                <input type="password"  name="password" id="password" required>
+                <span>Password</span>
+                <i></i>
+            </div>
+            <div class="links">
+                <a href="">Forgot Password</a>
+                <a href="">Signup</a>
+            </div>
+            <input type="submit">
+        </form>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
